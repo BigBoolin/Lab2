92b75bc14b7f41ae66562dad88616edabe2874af
eugenew2 <eugenew2@illinois.edu>
2018-01-31 16:45:41 -0600

    MAAAAAAAAAAAAAAZE.

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -34,7 +34,12 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
-            // Implement your maze solving algorithm here
+            if (maze.isFinished()== false)
+              maze.move();
+            if (maze.canMove()== false)
+                maze.turnRight();
+                maze.move();
+
         }
 
         if (maze.isFinished()) {
